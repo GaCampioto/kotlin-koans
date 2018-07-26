d78f114371f27abd4a77b706b9fe0574a1b7f960
Gabriel Campioto Pires <gcampioto@gmail.com>
2018-07-26 18:26:29 -0300

    fix: ii_collections

--- src/ii_collections/n17MaxMin.kt
+++ src/ii_collections/n17MaxMin.kt
@@ -5,12 +5,11 @@ fun example4() {
     val longestString = listOf("a", "b").maxBy { it.length }
 }
 
-fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
-    // Return a customer whose order count is the highest among all customers
-    todoCollectionTask()
-}
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = customers
+        .sortedByDescending { it.orders.size }
+        .first()
 
-fun Customer.getMostExpensiveOrderedProduct(): Product? {
-    // Return the most expensive product which has been ordered
-    todoCollectionTask()
-}
+fun Customer.getMostExpensiveOrderedProduct(): Product? = orders
+        .flatMap { it.products }
+        .sortedByDescending { it.price }
+        .first()
