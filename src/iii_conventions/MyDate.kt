f9550c644d9bbea7404678f48303e399d22d49c3
Gabriel Campioto Pires <gcampioto@gmail.com>
2018-08-01 16:06:54 -0300

    fix: iii_conventions - missing 1 test

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,13 +1,46 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import com.sun.org.apache.xpath.internal.operations.Bool
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate (val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+operator fun MyDate.plus(timeInterval: TimeInterval) = addTimeIntervals(timeInterval, 1)
+operator fun MyDate.plus(bunchOfTimeInterval: BunchOfTimeInterval) = addTimeIntervals(bunchOfTimeInterval.timeInterval, bunchOfTimeInterval.number)
 
 enum class TimeInterval {
     DAY,
     WEEK,
     YEAR
 }
+operator fun TimeInterval.times(number: Int) = BunchOfTimeInterval(this, number)
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(override val start: MyDate,
+        override val endInclusive: MyDate) :
+        ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> = DateIterator(this)
+
+    override fun contains(value: MyDate): Boolean = start <= value && value <= endInclusive
+}
+
+class DateIterator(dateRange: DateRange) : Iterator<MyDate> {
+    var current: MyDate = dateRange.start
+    var endInclusive: MyDate = dateRange.endInclusive
+
+    override fun hasNext(): Boolean = current <= endInclusive
+
+    override fun next(): MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
+    }
+
+}
+
+class BunchOfTimeInterval(val timeInterval: TimeInterval, val number: Int)
